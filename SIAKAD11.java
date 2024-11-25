bfd2d27109d8fd2e122f6c0c9d4d3854b55da444
rifat syamlan <rifatsyamlan78@gmail.com>
2024-11-25 19:42:51 +0700

    commit

--- SIAKAD11.java
+++ SIAKAD11.java
@@ -3,29 +3,34 @@ public class SIAKAD11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[][] nilai = new int[4][3];
+        System.out.print("Masukkan jumlah siswa: ");
+        int jumlahSiswa = sc.nextInt();
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlahMataKuliah = sc.nextInt();
 
-        for(int i = 0; i < nilai.length; i++){
-            System.out.print("Input nilai mahasiswa ke-" + (i+1));
+        int[][] nilai = new int[jumlahSiswa][jumlahMataKuliah];
+
+        for(int i = 0; i < jumlahSiswa; i++){
+            System.out.print("Input nilai mahasiswa ke-" + (i+1) + ": ");
             double totalPerSiswa = 0;
 
-            for(int j = 0; j < nilai[i].length; j++){
+            for(int j = 0; j < jumlahMataKuliah; j++){
                 System.out.println("\nNilai mata kuliah " + (j+1) + ": ");
-                nilai[j][i] = sc.nextInt();
-                totalPerSiswa += nilai[j][i];
+                nilai[i][j] = sc.nextInt();
+                totalPerSiswa += nilai[i][j];
             }
-            System.out.println("Nilai rata - rata: " + totalPerSiswa/3);
+            System.out.println("Nilai rata - rata: " + totalPerSiswa/jumlahMataKuliah);
         }
 
-        System.out.println("\n ============================");
+        System.out.println("\n============================");
         System.out.println("Rata - rata nilai setiap mata kuliah: ");
 
-        for(int j = 0; j < 3; j++){
+        for(int j = 0; j < jumlahMataKuliah; j++){
             double totalPerMatkul = 0;
-            for(int i = 0; i < 4; i++){
+            for(int i = 0; i < jumlahSiswa; i++){
                 totalPerMatkul += nilai[i][j];
             }
-            System.out.println("Mata kuliah " + (j+1) + ": " + totalPerMatkul /4);
+            System.out.println("Mata kuliah " + (j+1) + ": " + totalPerMatkul / jumlahSiswa);
         }
     }
 }
